e0476ab41e09f4ad4304bfce1c0ca09cfa8302a8
ssol-park <ksttlov.naver@hanmail.net>
2024-03-04 13:11:23 +0900

    Feat: added WebClientConfig and need to check TODO comments

---
+++ src/main/java/com/webflux/study/client/WebClientConfig.java
@@ -0,0 +1,47 @@
+package com.webflux.study.client;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Configuration
+public class WebClientConfig {
+
+    DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
+
+    @Bean
+    public WebClient webClient() {
+
+        // timeout
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofMillis(5000))
+                .doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+                        .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)))
+                ;
+
+        return WebClient.builder()
+                .uriBuilderFactory(factory)
+                .baseUrl("") // TODO
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .filter(ExchangeFilterFunction.ofRequestProcessor(request -> Mono.just(request))) // TODO Req log filter
+                .filter(ExchangeFilterFunction.ofResponseProcessor(response -> Mono.just(response))) // TODO Res log filter
+                .defaultHeader("Content-type", MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .build();
+    }
+
+}
